1ac29bf275ea59194f0a2f59f4a4577b5664bd64
Dzhanaur1 <3923328@gmail.com>
2024-05-10 12:00:01 +0300

    fix getProducts

--- src/app/utils/getProducts.tsx
+++ src/app/utils/getProducts.tsx
@@ -1,5 +1,7 @@
 export const getProudcts = async (category: string) => {
   const baseURL = window.location.origin;
-  const products = await fetch(`${baseURL}/api/products/${category}`);
+  const products = await fetch(
+    `https://utopia-git-master-dzhanaur1s-projects.vercel.app/api/products/${category}`
+  );
   return products.json();
 };
